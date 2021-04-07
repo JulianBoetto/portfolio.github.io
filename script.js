989364dc40a1b639f2f7077b4cd2a6889d86c6e1
julián Boetto <malostratoscba@gmail.com>
2021-04-07 17:45:39 -0300

    responsividade perfil + icons

--- script.js
+++ script.js
@@ -111,34 +111,6 @@ function link2() {
 function link3() {
     window.open(showData[4].html_url);
 }
-// link1 = "https://github.com/JulianBoetto/Blogdaviagem.github.io"
-
-// const cep = document.querySelector("#cep")
-
-// const showData = (result)=>{
-//     for(const campo in result){
-//         if(document.querySelector("#"+campo)){
-//             document.querySelector("#"+campo).value = result[campo]
-//         }
-//     }
-// }
-
-// cep.addEventListener("blur",(e)=>{
-//     let search = cep.value.replace("-","")
-//     const options = {
-//         method: 'GET',
-//         mode: 'cors',
-//         cache: 'default'
-//     }
-
-//     fetch(`https://viacep.com.br/ws/${search}/json/`, options)
-//     .then(response =>{ response.json()
-//         .then( data => showData(data))
-//     })
-//     .catch(e => console.log('Deu Erro: '+ e,message))
-// })
-
-
 
 //FORMULARIO
 
