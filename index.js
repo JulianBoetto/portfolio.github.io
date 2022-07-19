6a2446582ad6a82cb6daa710c9d3524ecb26e44f
Julián Boetto <julib_8724@hotmail.com>
2022-07-20 01:00:51 +0200

    Dados do formulario pronto

---
+++ index.js
@@ -0,0 +1,7 @@
+const form = document.contactForm
+
+form.addEventListener("submit", getForm)
+
+function getForm() {
+    document.querySelectorAll(".form-control").forEach(form => console.log(form.value))
+}
