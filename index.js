bf156d7680ef79afce25d009447fc8a8abdb9e7c
Julián Boetto <julib_8724@hotmail.com>
2023-04-04 14:13:43 -0300

    Funcionando los botones up y next section

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const userLang = navigator.language || navigator.userLanguage;
 const buttonEs = document.getElementById("btnEs");
 const buttonPt = document.getElementById("btnPt");
 
+const btnTop = document.getElementById("btn-top");
+const btnNextSection = document.getElementById("btn-next");
+
+const windowHeight = window.innerHeight;
+const windowCenter = windowHeight / 2;
+let currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+const sections = document.querySelectorAll('section');
+
+
 switch (userLang) {
   case "es-AR":
     for (var i = 0; i < es_language.length; i++) {
@@ -52,18 +61,19 @@ buttonEs.addEventListener("click", changeEs);
 buttonPt.addEventListener("click", changePt);
 
 // Volver arriba
-const btnTop = document.getElementById("btn-top");
-const btnNextSection = document.getElementById("btn-next");
-let scrollPos = window.pageYOffset || document.documentElement.scrollTop;
 function updateScrollPos() {
-  scrollPos = window.pageYOffset || document.documentElement.scrollTop;
+  currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
 }
 
 function toggleBtnTop() {
-  if (scrollPos > 0) {
+  if (currentScrollPosition > 0) {
     btnTop.style.display = "block";
-    btnNextSection.style.display = "none";
-  } else if (scrollPos === 0) {
+    if (currentScrollPosition > (windowHeight / 1.07) && currentScrollPosition < (windowHeight * 1.07 )) {
+      btnNextSection.style.display = "block";
+    } else {
+      btnNextSection.style.display = "none";
+    }
+  } else if (currentScrollPosition === 0) {
     btnNextSection.style.display = "block";
     btnTop.style.display = "none";
   } else {
@@ -71,32 +81,54 @@ function toggleBtnTop() {
   }
 }
 
+window.addEventListener("scroll", function () {
+  updateScrollPos();
+  toggleBtnTop();
+});
+
+let index = 1;
 function scrollToTop() {
+  index = 1;
   window.scrollTo({
     top: 0,
     behavior: "smooth",
   });
 }
 
-window.addEventListener("scroll", function () {
-  updateScrollPos();
-  toggleBtnTop();
-});
-
 toggleBtnTop();
 
 // Navegar a la seccion siguiente
-function scrollToNextSection() {
-  var currentSection =
-    document.querySelector(":target") || document.querySelector("section");
-const nextSeccion = currentSection.nextElementSibling;
-nextSeccion.scrollIntoView();
-const seccionTop = nextSeccion.getBoundingClientRect().top + window.pageYOffset;
-const windowHeight = window.innerHeight;
-const seccionHeight = nextSeccion.offsetHeight;
-const seccionOffset = (windowHeight - seccionHeight) / 2;
-window.scrollTo({
-  top: seccionTop - seccionOffset,
-  behavior: 'smooth'
+// function scrollToNextSection() {
+//   var currentSection =
+//     document.querySelector(":target") || document.querySelector("section");
+//     const nextSeccion = currentSection.nextElementSibling;
+//     console.log(currentSection, nextSeccion)
+// nextSeccion.scrollIntoView();
+// const seccionTop = nextSeccion.getBoundingClientRect().top + window.pageYOffset;
+// const windowHeight = window.innerHeight;
+// const seccionHeight = nextSeccion.offsetHeight;
+// const seccionOffset = (windowHeight - seccionHeight) / 2;
+// window.scrollTo({
+//   top: seccionTop - seccionOffset,
+//   behavior: 'smooth'
+// });
+// }
+
+
+const nextButton = document.getElementById("btn-next");
+
+window.addEventListener("scroll", () => {
+  const { top } = sections[0].getBoundingClientRect();
+  if (top <= 0) {
+    sections[0].classList.add("fixed");
+  } else {
+    sections[0].classList.remove("fixed");
+  }
 });
-}
+
+nextButton.addEventListener("click", () => {
+  sections[index].scrollIntoView({ behavior: "smooth" });
+  index++
+});
+
+
