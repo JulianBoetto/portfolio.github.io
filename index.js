e7f5d10d96e7406cf8ae9addebc1103a40ceb620
Julián Boetto <julib_8724@hotmail.com>
2023-04-07 23:31:29 -0300

    Perfil con español y portugues

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-const es_language = document.getElementsByClassName("es-AR");
-// const en_language = document.getElementsByClassName("en");
-const pt_language = document.getElementsByClassName("pt-BR");
+const selectLang = document.querySelectorAll(".btnLanguage");
+const imgsLang = document.querySelectorAll(".imgLang");
 const userLang = navigator.language || navigator.userLanguage;
-
-const regex = /^pt/;
-
-const buttonEs = document.getElementById("btnEs");
-const buttonPt = document.getElementById("btnPt");
+const regexPt = /^pt/;
+const regexEs = /^es/;
 const btnTop = document.getElementById("btn-top");
 const nextButtons = document.querySelectorAll(".btn-effect");
 
@@ -14,46 +10,23 @@ let currentScrollPosition = window.pageYOffset;
 const windowHeight = window.innerHeight;
 const sections = document.querySelectorAll('section');
 
-switch (userLang) {
-  case "es-AR":
-    for (var i = 0; i < es_language.length; i++) {
-      es_language[i].style.display = "inline";
-      // en_language.style.display = "none";
-      pt_language[i].style.display = "none";
-    }
-    break;
-
-  case "en":
-    for (var i = 0; i < es_language.length; i++) {
-      es_language[i].style.display = "none";
-      // en_language.style.display = "inline";
-      pt_language[i].style.display = "none";
-    }
-    break;
-
-  default:
-    for (var i = 0; i < es_language.length; i++) {
-      es_language[i].style.display = "none";
-      // en_language.style.display = "none";
-      pt_language[i].style.display = "inline";
-    }
-    break;
-}
-
-function changeEs() {
-  for (var i = 0; i < es_language.length; i++) {
-    es_language[i].style.display = "inline";
-    // en_language.style.display = "none";
-    pt_language[i].style.display = "none";
+changeLang(userLang);
+function changeLang(userLang) {
+  if(regexEs.test(userLang)) {
+    loadLanguage("es");
+  } else if (regexPt.test(userLang)) {
+    loadLanguage("pt");    
+  } else {
+    loadLanguage("en");
   }
-}
 
-function changePt() {
-  for (var i = 0; i < es_language.length; i++) {
-    es_language[i].style.display = "none";
-    // en_language.style.display = "none";
-    pt_language[i].style.display = "inline";
-  }
+  imgsLang.forEach((btn, index) => {
+    if(btn.id === userLang) {
+      imgsLang[index].style.display = "inline";
+    } else {
+      imgsLang[index].style.display = "none";
+    }
+  })
 }
 
 function scrollToTop() {
@@ -61,7 +34,6 @@ function scrollToTop() {
   currentScrollPosition = 0;
 }
 
-// Navegar a la seccion siguiente
 function scrollToNextSection() {
   let targetSection = null;
   
@@ -99,8 +71,54 @@ window.addEventListener('resize', function() {
   showBtnTop();
 });
 
-buttonEs.addEventListener("click", changeEs);
-buttonPt.addEventListener("click", changePt);
+selectLang.forEach((button) => button.addEventListener('click', () => {
+  changeLang(button.attributes["value"].value)
+}));
+
 nextButtons.forEach((button) => button.addEventListener('click', () => {
   scrollToNextSection()
 }));
+
+function loadLanguage(lang) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var translations = parseTranslations(this.responseText);
+      updateText(translations);
+    }
+  };
+  xhttp.open("GET", "lang/" + lang + ".txt", true);
+  xhttp.send();
+}
+
+function parseTranslations(translationsString) {
+  var translations = {};
+  var lines = translationsString.split("\n");
+  for (var i = 0; i < lines.length; i++) {
+    var line = lines[i];
+    if (line.trim().length > 0 && !line.startsWith("#")) {
+      var parts = line.split("=");
+      translations[parts[0]] = parts[1];
+    }
+  }
+  return translations;
+}
+
+function updateText(translations) {
+  var elements = document.querySelectorAll('[data-i18n]');
+  for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+    var keys = element.getAttribute('data-i18n').split(';');
+    for (var j = 0; j < keys.length; j++) {
+      var key = keys[j];
+      var value = translations[key];
+      if (value) {
+        if (j === 0) {
+          element.textContent = value;
+        } else {
+          element.textContent += ' ' + value;
+        }
+      }
+    }
+  }
+}
